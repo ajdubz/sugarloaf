f3bc3f06fd7bd887e099cc9ae8f2f2fb9bd22b48
Alan W <wildauer07@gmail.com>
2025-09-11 18:41:16 -0400

    style: update header background color to transparent and remove logo image style: enhance body background with banner image and adjust MainPage hero styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,8 @@
 import "./Header.css";
-import logoUrl from "../../assets/originals/logo_sugarloaf.jpeg";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header-logo">
-        <img src={logoUrl} alt="Sugarloaf Mountain Ranch logo" />
-      </div>
       <nav className="header-nav">
         <ul>
           <li><a href="#">Home</a></li>
